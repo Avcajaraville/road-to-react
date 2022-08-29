9408617c709b73ffd18b01adc15cb39193228b59
avcajaraville <avcajaraville@gmail.com>
2022-08-29 16:43:12 +0200

    Expanding intial example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import * as React from 'react';
 
+const welcome = {
+  title: 'React',
+  greeting: 'Hey'
+};
+
+const reasons = [
+  'get a new job',
+  'learn new technologies',
+];
+
+function getTitle(title) {
+  return title;
+}
+
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
+      <h1>{welcome.greeting} {getTitle(welcome.title)}!</h1>
+      <ul>
+        {reasons.map((reason, index) => {
+          return <li key={index}>{reason}</li>
+        })}
+      </ul>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text" />
     </div >
   );
 }
