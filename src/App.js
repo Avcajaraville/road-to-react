8b6d0860b89d617ea559975bd0df508eaf1f32af
avcajaraville <avcajaraville@gmail.com>
2022-08-29 16:56:43 +0200

    Add example for lists/array

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import * as React from 'react';
 
-const welcome = {
+const title = 'My hacker stories';
+
+const list = [{
   title: 'React',
-  greeting: 'Hey'
-};
-
-const reasons = [
-  'get a new job',
-  'learn new technologies',
-];
-
-function getTitle(title) {
-  return title;
-}
+  url: 'https://reactjs.org',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectId: 0,
+}, {
+  title: 'Redux',
+  url: 'https://redux.js.org',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectId: 1,
+}];
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting} {getTitle(welcome.title)}!</h1>
-      <ul>
-        {reasons.map((reason, index) => {
-          return <li key={index}>{reason}</li>
-        })}
-      </ul>
+      <h1>{title}</h1>
       <label htmlFor="search">Search:</label>
       <input id="search" type="text" />
+      <ul>
+        {list.map((item, index) =>
+          <li key={item.objectId}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        )}
+      </ul>
     </div >
   );
 }
