e60d2ec962136f278b8b77e3f8190573ecc94d95
avcajaraville <avcajaraville@gmail.com>
2022-11-08 16:34:32 +0100

    Move state to parent component (lifting state)

--- src/App.js
+++ src/App.js
@@ -1,75 +1,78 @@
-import * as React from 'react';
+import * as React from "react";
 
 const Search = (props) => {
-  console.log('Rendering Search');
-  const [searchTerm, setSeachTerm] = React.useState('');
-  const handleChange = (event) => {
-    setSeachTerm(event.target.value)
-    props.onSearch(event);
-  };
-
   return (
     <div>
       <label htmlFor="search">Search:</label>
-      <input id="search" type="text" onChange={handleChange} onBlur={handleChange} />
-      <p>
-        Seaching for <strong>{searchTerm}</strong>
-      </p>
+      <input
+        id="search"
+        type="text"
+        onChange={props.onSearch}
+      />
     </div>
   );
-
-}
+};
 
 const List = (props) => {
-  console.log('Rendering List');
-  return <ul>
-    {props.list.map((item) =>
-      <Item key={item.objectId} item={item} />
-    )}
-  </ul>
-}
+  return (
+    <ul>
+      {props.list.map((item) => (
+        <Item key={item.objectId} item={item} />
+      ))}
+    </ul>
+  );
+};
 
 const Item = (props) => {
-  console.log('Rendering Item');
-  return <li>
-    <span>
-      <a href={props.item.url}>{props.item.title}</a>
-    </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
-  </li>
-}
+  return (
+    <li>
+      <span>
+        <a href={props.item.url}>{props.item.title}</a>
+      </span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
+    </li>
+  );
+};
 
 const App = () => {
-  console.log('Rendering App');
-  const title = 'My hacker stories';
-  const stories = [{
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectId: 0,
-  }, {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectId: 1,
-  }];
+  const title = "My hacker stories";
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectId: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectId: 1,
+    },
+  ];
+
+  const [searchTerm, setSeachTerm] = React.useState("");
   const handleChange = (event) => {
-    console.log(event.target.value);
+    setSeachTerm(event.target.value);
   };
+  const searchedStories = stories.filter(story => searchTerm && story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <div>
       <h1>{title}</h1>
-      <Search onSearch={handleChange}/>
-      <List list={stories} />
-    </div >
+      <Search onSearch={handleChange} />
+      <p>
+        Seaching for: <strong>{searchTerm}</strong>
+      </p>
+      <List list={searchedStories} />
+    </div>
   );
-}
+};
 
 export default App;
