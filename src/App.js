101ea0d08927d069ed5d86da191b87f147a50f7a
avcajaraville <avcajaraville@gmail.com>
2022-11-18 18:20:21 +0100

    Add async behaviour to sample list

--- src/App.js
+++ src/App.js
@@ -12,24 +12,33 @@ const useLocalStorageState = (key, initialValue) => {
   return [value, setValue];
 };
 
-const initialStories = [
-  {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectId: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectId: 1,
-  },
-];
+const getAsyncStories = async () =>
+  new Promise((resolve) => {
+    window.setTimeout(() => {
+      resolve({
+        data: {
+          stories: [
+            {
+              title: "React",
+              url: "https://reactjs.org",
+              author: "Jordan Walke",
+              num_comments: 3,
+              points: 4,
+              objectId: 0,
+            },
+            {
+              title: "Redux",
+              url: "https://redux.js.org",
+              author: "Dan Abramov, Andrew Clark",
+              num_comments: 2,
+              points: 5,
+              objectId: 1,
+            },
+          ],
+        },
+      });
+    }, 3000);
+  });
 
 const App = () => {
   const title = "My hacker stories";
@@ -37,7 +46,13 @@ const App = () => {
   const handleSearch = (event) => {
     setSeachTerm(event.target.value);
   };
-  const [stories, setStories] = React.useState(initialStories);
+  const [stories, setStories] = React.useState([]);
+  React.useEffect(() => {
+    (async () => {
+      const response = await getAsyncStories();
+      setStories(response.data.stories);
+    })();
+  }, []);
   const searchedStories = stories.filter(
     (story) =>
       searchTerm && story.title.toLowerCase().includes(searchTerm.toLowerCase())
